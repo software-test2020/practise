8759a9efbe8cfd52d3d2005d2adba16af713e000
software-test2020 <shakeelerfani09@gmail.com>
2021-04-03 17:50:38 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] d = { 3, 5, 4, 6 };
+
+            Console.WriteLine(counter(d));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+       public static int  counter(int[] array)
+        {
+
+
+           int b= array.Sum();
+
+            return b;
+
+            
+
+
+
+       }
+    }
+}
